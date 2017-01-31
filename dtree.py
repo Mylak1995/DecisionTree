fffdc1f6d6437ec31c2ac04128f0746e6cd8887c
MacIej Mylik <maciej.mylik16@imperial.ac.uk>
2017-01-31 22:26:56 +0000

    Second

---
+++ dtree.py
@@ -0,0 +1,38 @@
+import scipy.io as sio
+
+my_data1=sio.loadmat('cleandata_students.mat')
+
+data1=my_data1['x']
+classification1=my_data1['y']
+
+my_data2=sio.loadmat('noisydata_students.mat')
+
+data2=my_data2['x']
+classification2=my_data2['y']
+
+
+class tree:
+    def __init__(self,op,kids=[],classification=None):
+        self.op=op
+        self.kids=kids
+        self.classification=classification
+
+
+def to_binary_classifier(classifier,number):
+    for x in range(0,len(classifier)):
+        if classifier[x][0] == number:
+            classifier[x][0] = 1
+        else:
+            classifier[x][0] = 0
+    return classifier
+
+
+
+
+
+
+
+
+
+
+
