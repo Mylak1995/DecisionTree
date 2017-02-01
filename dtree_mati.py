0d8eafd2bdbb3796153b1b6481d81ca472a549bc
Mateusz Staniszewski <mjs114@ray14.doc.ic.ac.uk>
2017-02-01 12:12:42 +0000

    update dtree_mati with gain function; choose function next

--- dtree_mati.py
+++ dtree_mati.py
@@ -13,7 +13,7 @@ classification2=my_data2['y']
 
 
 class tree:
-    def __init__(self,op,kids=[],classification=None):
+    def __init__(self,op=None,kids=[],classification=None):
         self.op=op
         self.kids=kids
         self.classification=classification
@@ -23,18 +23,49 @@ class tree:
             return True
         return False
 
-    #def decision_tree_learning(examples, attr, bin_targets):
-    #    if
+    def majority_value(bin_targets):
+        if np.sum(bin_targets)>(len(bin_targets)/2):
+            return 1
+        return 0
 
 
-def to_binary_classifier(classifier,number):
-    list=[]
-    for x in range(0,len(classifier)):
-        if classifier[x][0] == number:
-            list.append(1)
+    def choose_best_decision_attribute(examples,attributes,binary_targets):
+        #for i in attributes:
+            
+        return true
+
+    def entropy(p,n):
+        from math import log
+        a=float(p)/(p+n)
+        b=float(n)/(p+n)
+        log2 = lambda x: log(x)/log(2)
+        return (-a*log2(a)-b*log2(b))
+
+    #input of p and n?
+    #define p0,n0,p1,n1
+    def remainder(attributes):
+        #something p0,p1 =
+        a=float(p0+n0)/(p+n)
+        b=float(p1+n1)/(p+n)
+        return a*entropy(p0,n0)+b*entropy(p1,n1)
+
+    def gain(attributes,p,n):
+        return entropy(p,n)-remainder(attributes)
+
+    def decision_tree_learning(examples,attributes,bin_targets):
+        if is_unique(bin_targets):
+            return tree(classification=bin_targets[0])
+        elif examples.size==0:
+            return tree(classification=majority_value(bin_targets))
         else:
-            list.append(0)
-    return list
+            return True
 
-print(to_binary_classifier(classification1,1))
-print(classification1)
+
+    def to_binary_classifier(classifier,number):
+        list=[]
+        for x in range(0,len(classifier)):
+            if classifier[x][0] == number:
+                list.append(1)
+            else:
+                list.append(0)
+        return list
