9455286663e7de77baafb227b6a81b69b8c44bc4
Maciej Mylik <mmm615@ray15.doc.ic.ac.uk>
2017-02-01 10:53:42 +0000

    WIP

--- dtree.py
+++ dtree.py
@@ -1,18 +1,20 @@
 import scipy.io as sio
+import numpy as np
 
 my_data1=sio.loadmat('cleandata_students.mat')
-
 data1=my_data1['x']
 classification1=my_data1['y']
 
-my_data2=sio.loadmat('noisydata_students.mat')
 
+my_data2=sio.loadmat('noisydata_students.mat')
 data2=my_data2['x']
 classification2=my_data2['y']
 
 
+
+print(data2.size)
 class tree:
-    def __init__(self,op,kids=[],classification=None):
+    def __init__(self,op=None,kids=[],classification=None):
         self.op=op
         self.kids=kids
         self.classification=classification
@@ -27,14 +29,34 @@ def to_binary_classifier(classifier,number):
             list.append(0)
     return list
 
-print(to_binary_classifier(classification1,1))
-print(classification1)
+
+def is_unique(bin_targets):
+    if len(bin_targets)==np.sum(bin_targets) or np.sum(bin_targets)==0:
+        return True
+    return False
+
+def majority_value(bin_targets):
+    if np.sum(bin_targets)>(len(bin_targets)/2):
+        return 1
+    return 0
 
 
+def choose_best_decision_attribute(examples,attributes,binary_targets):
+    return true
 
 
+def entropy(p,n):
+    from math import log
+    a=float(p)/(p+n)
+    b=float(n)/(p+n)
+    log2 = lambda x: log(x)/log(2)
+    return (-a*log2(a)-b*log2(b))
 
 
-
-
-
+def decision_tree_learning(examples,attributes,bin_targets):
+    if is_unique(bin_targets):
+        return tree(classification=bin_targets[0])
+    elif examples.size==0:
+        return tree(classification=majority_value(bin_targets))
+    else:
+        return True
